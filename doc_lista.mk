e04b13cff3cb491f47b13f1cf41dc91455f6113d
Arthur <arthurclementte@hotmail.com>
2019-12-07 11:48:28 -0300

    Adicionada a documentação do programa de manipulaçaõ de listas

---
+++ doc_lista.mk
@@ -0,0 +1,7 @@
+Programa destinado a criar e manipular uma lista com **números inteiros**
+com as opções de:
+* Iniciar a lista informando o tamanho dela
+* Recuperar um valor já adicionado
+* Imprimir a lista completa
+* Deletar um valor desejado
+* Finalizar o programa
